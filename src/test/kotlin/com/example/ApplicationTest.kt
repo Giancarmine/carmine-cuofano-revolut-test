b5aea2a2a6b9a8e5bc95bfb043317f0289bdbca1
El_Giancar <giancarmine.cuofano@gmail.com>
2022-10-10 00:45:03 +0200

    Add tests

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -3,6 +3,7 @@ package com.example
 import com.example.plugins.DatabaseFactory
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
+import com.example.service.UserService
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
@@ -10,16 +11,37 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
+    private val userService = UserService()
+
     @Test
-    fun testRoot() = testApplication {
+    fun config() = testApplication {
         application {
             DatabaseFactory.init()
             configureSerialization()
             configureRouting()
         }
-        //client.get("/hello/pippo").apply {
-        //    assertEquals(HttpStatusCode.OK, status)
-            // assertEquals("Hello World!", bodyAsText())
-        //}
+
     }
+
+    @Test
+    fun testNotUserPresent() = testApplication {
+        client.get("/hello/rvlTest").apply {
+            userService.deleteAll()
+            assertEquals(HttpStatusCode.NoContent, status)
+        }
+    }
+
+//    @Test
+//    fun testAddAUser() = testApplication {
+//        client.put("/hello/rvlTest") {
+//            headers {
+//                contentType(ContentType.Application.Json)
+//                setBody(DateOfBirthDTO(LocalDate.now().toString()).toString())
+//            }
+//        }.apply {
+//            println("body: " + body())
+//            println("bodyAsText: " + bodyAsText())
+//            assertEquals(HttpStatusCode.Created, status)
+//        }
+//    }
 }
